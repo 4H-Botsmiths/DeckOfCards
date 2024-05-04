41cf9169d66cf0cb1aacf48b35e82f196f7635ab
pauldalyiii <162659949+pauldalyiii@users.noreply.github.com>
2024-05-04 15:39:47 -0700

    update app to use deck

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        var deck = new Deck();
+        deck.Shuffle();
+
+        while(!deck.IsEmpty())
+            System.out.println(deck.TakeCard() + "; " + deck.RemainingCardCount() + " cards remaining");
     }
 }
