c18f11b2b79b5f8552f7d80d49c2565689579401
Paul Daly III <Paul.daly.iii@outlook.com>
2024-05-04 12:04:47 -0700

    I created the card class.

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+public class Card {
+    private Rank _rank;
+    private Suit _suit;
+
+    public Card(Rank rank, Suit suit) {
+        super();
+         _rank = rank;
+         _suit = suit;
+    }
+    public String toString() {
+        return this._rank + " of " + this._suit;
+    }
+}
