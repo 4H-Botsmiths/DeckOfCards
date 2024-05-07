a9151cf74b9742200b2f8b1ae1d465768ec9bef2
Paul Daly III <paul.daly.iii@outlook.com>
2024-05-06 18:52:05 -0700

    removed hello world, removed shuffle, and made it tell you how many cards are in the deck

--- src/App.java
+++ src/App.java
@@ -1,9 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
         var deck = new Deck();
-        deck.Shuffle();
+
+        System.out.println(deck.RemainingCardCount() + " cards in this deck");
 
         while(!deck.IsEmpty())
             System.out.println(deck.TakeCard() + "; " + deck.RemainingCardCount() + " cards remaining");
