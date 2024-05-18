b52526d3360686f79be271985a590f4018d921a0
Charlie Edson <canIborrowyourkideys@email.com>
2024-05-18 10:13:27 -0700

    ChangedNamesToUnicode

--- src/Suit.java
+++ src/Suit.java
@@ -1,8 +1,8 @@
 public enum Suit {
-    HEARTS("Hearts"),
-    SPADES("Spades"),
-    DIAMONDS("Diamonds"),
-    CLUBS("Clubs");
+    HEARTS("♥"),
+    SPADES("♠"),
+    DIAMONDS("♦"),
+    CLUBS("♣");
 
     private final String value;
 
