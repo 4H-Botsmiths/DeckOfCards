9cd0aab65fe7e94dc1dd55d0ea8df6e6bb0b46f8
Kolton Bloom <bloomkd46@icloud.com>
2024-05-18 09:50:07 -0700

    Added Hand class

---
+++ src/Hand.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class Hand {
+  private Stack<Card> cards;
+
+  public void addCard(Card card) {
+    cards.push(card);
+  }
+
+  public void addCards(Stack<Card> cards) {
+    this.cards.addAll(cards);
+  }
+
+  public Card takeCard() {
+    return cards.pop();
+  }
+
+  public boolean isEmpty() {
+    return cards.isEmpty();
+  }
+
+  public int remainingCards() {
+    return cards.size();
+  }
+}
