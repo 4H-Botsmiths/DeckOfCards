5c8e5d74b633887daf13c3f60f4e7c4c8c4a3efc
bri <brian.overlin@outlook.com>
2024-05-18 10:13:33 -0700

    Added the Deck class

---
+++ src/Deck.java
@@ -0,0 +1,35 @@
+import java.util.Collections;
+import java.util.Stack;
+
+public class Deck {
+    private Stack<Card> cards = new Stack<Card>(); 
+
+    public Deck() {
+        BuildDeck();
+        Shuffle();
+    
+    }
+    private void BuildDeck() {
+        for(Suit suit : Suit.values()) {
+            for(Rank rank: Rank.values()) {
+                this.cards.push(new Card(rank, suit));
+            }
+        }
+    }
+
+    public void Shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public boolean IsEmpty(){
+        return this.cards.size() == 0;
+    }
+
+    public int ReminingCardCount(){
+        return this.cards.size();
+    }
+
+    public Card TakeCard(){
+        return this.cards.pop();
+    }
+}
